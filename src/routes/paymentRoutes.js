b07b84060a3a18c3180f2622f5e19fa63350d0b6
unknown <aleyladely5@gmail.com>
2026-08-29 13:12:52 +0300

    Add /payments/initiate endpoint for student payment functionality

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post('/mpesa/callback', paymentController.mpesaCallback);
 router.post('/stk-push', protect, paymentLimiter, paymentController.initiateSTK);
+router.post('/initiate', protect, paymentLimiter, paymentController.initiateSTK);
 router.get('/', protect, paymentController.list);
 router.get('/:id', protect, paymentController.getById);
 
